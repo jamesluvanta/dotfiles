a04794224569fd686cd3199c50fb77af8c7dd307
James Lu <78759932+jamesluvanta@users.noreply.github.com>
2022-02-07 10:00:37 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 alias mgt='make generate-types'
-alias nc='npm run --prefix ~/code build'
+alias nc='npm run --prefix /workspaces/obsidian build'
 alias nuke='git branch | grep -v "main" | xargs git branch -D'
